88367678d291d1e7e1ed8f969bb8780dc4c4a88a
Panchagavi <46476682+Panchagavi@users.noreply.github.com>
2019-01-09 11:50:19 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World- For Sample Maven Project!";
 
     public App() {}
 
